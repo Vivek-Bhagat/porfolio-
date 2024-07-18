8d11fb9a81c45926e0b90267e408717ae3b85103
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-07-18 22:40:52 +0530

    created a project , preloader

---
+++ src/components/Header/nav/data.js
@@ -0,0 +1,41 @@
+export const links = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/",
+  },
+  {
+    title: "Projects",
+    href: "/",
+  },
+  {
+    title: "Careers",
+    href: "/",
+  },
+  {
+    title: "Contact",
+    href: "/",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "GitHub",
+    href: "/",
+  },
+  {
+    title: "LinkedIn",
+    href: "/",
+  },
+  {
+    title: "Instagram",
+    href: "/",
+  },
+  {
+    title: "Twitter",
+    href: "/",
+  },
+];
